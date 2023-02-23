9cc92611fa9bd46e7299b01664a8cc0adf7e8d94
Marcin Morys <marcin.m.morys@gmail.com>
2023-02-22 21:54:45 -0500

    add zip and fontconfig

--- install_apt_packages/install_utilities_WITH_SUDO.sh
+++ install_apt_packages/install_utilities_WITH_SUDO.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt install -y curl wget git build-essential linux-headers-$(uname -r) software-properties-common apt-transport-https tree xclip zsh
+sudo apt install -y curl wget git build-essential fontconfig linux-headers-$(uname -r) software-properties-common apt-transport-https tree xclip zip zsh
